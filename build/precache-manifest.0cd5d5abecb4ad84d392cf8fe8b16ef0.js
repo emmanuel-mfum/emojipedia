8a4377f639ba9e362230eb89e83ea1b9140e63c1
Emmanuel <55893421+emmanuel-mfum@users.noreply.github.com>
2020-08-20 15:49:48 -0400

    More files added

---
+++ build/precache-manifest.0cd5d5abecb4ad84d392cf8fe8b16ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961676dc86a6d6a3b9365716f78759e9",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "c88f225ea855adf83652",
+    "url": "/emojipedia/static/js/2.72bec74e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/emojipedia/static/js/2.72bec74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ffe0ab7500be522f13",
+    "url": "/emojipedia/static/js/main.6deb2303.chunk.js"
+  },
+  {
+    "revision": "1226d6881b6604b61d21",
+    "url": "/emojipedia/static/js/runtime-main.5379d3c5.js"
+  }
+]);
